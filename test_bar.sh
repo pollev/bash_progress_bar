422eee3f2f47591ff7ce3f152687d3430ab9eba4
Polle Vanhoof <vanhoofpolle@gmail.com>
2018-12-10 16:45:12 +0100

    Update test_bar.sh

--- test_bar.sh
+++ test_bar.sh
@@ -10,7 +10,7 @@ function generate_some_output_and_sleep() {
     head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-'
     head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-'
     echo -e "\n\n------------------------------------------------------------------"
-    echo -e "\n\n Now sleeping one second \n\n"
+    echo -e "\n\n Now sleeping briefly \n\n"
     sleep 0.3
 }
 
