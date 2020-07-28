8d9a56cfecab95bb266cecf4b305126333397586
Christopher White <cwhite@d3engineering.com>
2020-07-28 08:47:09 -0400

    Add .editorconfig; add license to progress_bar.sh

--- progress_bar.sh
+++ progress_bar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://github.com/pollev/bash_progress_bar - See license at end of file
 
 # Usage:
 # Source this script
@@ -9,7 +10,6 @@
 # block_progress_bar 45 <- turns the progress bar yellow to indicate some action is requested from the user
 # draw_progress_bar 90 <- advance progress bar
 # destroy_scroll_area <- remove progress bar
-
 
 # Constants
 CODE_SAVE_CURSOR="\033[s"
@@ -171,3 +171,26 @@ printf_new() {
     v=$(printf "%-${num}s" "$str")
     echo -ne "${v// /$str}"
 }
+
+
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2018--2020 Polle Vanhoof
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
