1f0d1b911495468c1d0debfc93f6175d112c21e0
Benjamin Renard <brenard@zionetrix.net>
2023-04-24 23:43:54 +0200

    Restore test_bar.sh and put advanced features tests in test_advanced.sh script

--- test_advanced.sh
+++ test_advanced.sh
@@ -14,21 +14,27 @@ generate_some_output_and_sleep() {
     sleep 0.3
 }
 
+MAX=99
+[ -n "$1" ] && MAX=$1
 
 main() {
     # Make sure that the progress bar is cleaned up when user presses ctrl+c
     enable_trapping
+    # Enable ETA
+    ETA_ENABLED="true"
     # Create progress bar
-    setup_scroll_area
-    for i in {1..99}
+    setup_scroll_area "Processing" $MAX
+    # Compute middle for user input test
+    let MIDDLE=MAX/2
+    for i in $(seq 1 $MAX)
     do
-        if [ $i = 50 ]; then
+        if [ $i -eq $MIDDLE ]; then
             echo "waiting for user input"
             block_progress_bar $i
             read -p "User input: "
         else
             generate_some_output_and_sleep
-            draw_progress_bar $i
+            draw_progress_bar $i "$( date "+%r" )| $i on $MAX"
         fi
     done
     destroy_scroll_area
